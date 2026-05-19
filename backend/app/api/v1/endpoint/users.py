48ab3c33f716c6d242e49826ef80ca5152c51fa1
Mark Ngo <mark.ngo203@gmail.com>
2026-05-19 02:12:11 -0400

    refactor(users): add response models to route handlers
    Refactors route handlers to return 'response_models' for API schemas.

--- backend/app/api/v1/endpoint/users.py
+++ backend/app/api/v1/endpoint/users.py
@@ -3,14 +3,14 @@ from fastapi import APIRouter, Depends, HTTPException
 from app.db.database import get_db
 from app.exceptions.user_exceptions import DuplicateUsernameException, UserNotFoundError
 from app.repositories.user_repository import UserRepository
-from app.schemas.user_dto import UserCreateRequest, UserUpdateRequest
+from app.schemas.user_dto import UserCreateRequest, UserUpdateRequest, UserResponse, UserUpdateResponse
 from app.services.user_service import UserService
 
 
 router = APIRouter()
 
 @router.post("/", status_code=201)
-def create_user(user: UserCreateRequest, db=Depends(get_db)):
+def create_user(user: UserCreateRequest, db=Depends(get_db)) -> UserResponse:
     try:
         service = UserService(UserRepository(db))
         created_user = service.create_user(user)
@@ -20,7 +20,7 @@ def create_user(user: UserCreateRequest, db=Depends(get_db)):
         raise HTTPException(status_code=409, detail=str(e))
 
 @router.get("/{user_id}", status_code=200)
-def get_user(user_id: int, db=Depends(get_db)):
+def get_user(user_id: int, db=Depends(get_db)) -> UserResponse:
     try:
         service = UserService(UserRepository(db))
         user = service.get_user(user_id)
@@ -30,7 +30,7 @@ def get_user(user_id: int, db=Depends(get_db)):
         raise HTTPException(status_code=404, detail=str(e))
 
 @router.put("/{user_id}", status_code=200)
-def update_user(user_id: int, user: UserUpdateRequest, db=Depends(get_db)):
+def update_user(user_id: int, user: UserUpdateRequest, db=Depends(get_db)) -> UserUpdateResponse:
     try:
         service = UserService(UserRepository(db))
         updated_user = service.update_user(user_id, user)
@@ -43,7 +43,7 @@ def update_user(user_id: int, user: UserUpdateRequest, db=Depends(get_db)):
 
 
 @router.delete("/{user_id}", status_code=204)
-def delete_user(user_id: int, db=Depends(get_db)):
+def delete_user(user_id: int, db=Depends(get_db)) -> None:
     try:
         service = UserService(UserRepository(db))
         service.delete_user(user_id)
